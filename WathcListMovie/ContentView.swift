4c985d6acd28cb5361be9f7b2dc14f9ae2bd54d4
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2023-06-19 10:44:39 +0800

    Initial Commit

---
+++ WathcListMovie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WathcListMovie
+//
+//  Created by Jaime Jazareno IV on 6/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WathcListMovie/WathcListMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WathcListMovieApp.swift
+//  WathcListMovie
+//
+//  Created by Jaime Jazareno IV on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WathcListMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
