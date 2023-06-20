02fa5c514c932cea155f4d2da8ac0598ecb42251
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2023-06-21 01:57:35 +0800

    Add extensions

---
+++ WathcListMovie/Extensions/Double+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Double+Extensions.swift
+//  WathcListMovie
+//
+//  Created by Jaime Jazareno IV on 6/20/23.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
